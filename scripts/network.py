ea99607b46a406f5115bda5f3be9f23cbccd32f7
Juuso <juusokorhoin@gmail.com>
2019-09-07 16:45:53 +0300

    training works, it was down to learning rate and number of iterations. Also removed the unnecessary files from directory

--- scripts/network.py
+++ scripts/network.py
@@ -2,104 +2,84 @@ import numpy as np
 
 
 class NeuralNetwork:
-    # for example new NeuralNetwork(4,[8,8],2): 4 inputs, 2x 8 node hidden layers, 2 node output
-    def __init__(self, input_layer, hidden_layers, output_layer):
-        self.input_layer = input_layer
-        self.hidden_layers = hidden_layers
-        self.output_layer = output_layer
-        # during the feedforward, we save both the nodes and the inputs (the zs') before sigmoid function
-        # for backpropagation purposes
+    # for example new NeuralNetwork([4, 8, 8, 2]): 4 inputs, 2x 8 node hidden layers, 2 node output
+    def __init__(self, layer_sizes):
+        self.layer_sizes = layer_sizes
+        self.num_layers = len(self.layer_sizes)
+        # initialization of biases and weights, np.random.randn: random numbers according to gaussian db
+        self.biases = [np.random.randn(l) for l in self.layer_sizes[1:self.num_layers]]
+        self.weights = [np.random.randn(l2, l1) for l1, l2 in zip(self.layer_sizes[0:self.num_layers - 1],
+                                                                  self.layer_sizes[1:self.num_layers])]
+        """during the feedforward of the input, we save both the activations and the inputs to layers (the zs)
+        for backpropagation purposes"""
         self.activations = []
         self.zs = []
-        # initializations of weights and biases
-        np.random.seed(3)
-        self.weights = self.initWeights()
-        self.biases = self.initBiases()
 
-    def initWeights(self):
-        weights = []
-        # first lets initialize the weights from input to first hidden layer
-        w_ih = np.random.randn(self.hidden_layers[0], self.input_layer)
-        weights.append(w_ih)
-        # then go over the hidden layers weights
-        for i in range(1, len(self.hidden_layers)):
-            w_hh = np.random.randn(self.hidden_layers[i], self.hidden_layers[i-1])
-            weights.append(w_hh)
-        # and at last from last hidden layer to output
-        w_ho = np.random.randn(self.output_layer, self.hidden_layers[len(self.hidden_layers)-1])
-        weights.append(w_ho)
-        return weights
-
-    def initBiases(self):
-        biases = []
-        for i in range(0, len(self.hidden_layers)):
-            bv = np.random.randn(self.hidden_layers[i])  # generates one bias-vector
-            biases.append(bv)
-        bv_o = np.random.randn(self.output_layer)
-        biases.append(bv_o)
-        return biases
     @staticmethod
     def cost_prime(outputs, targets):
-        return 2*(outputs - targets)
+        return 2*(outputs - targets)  # partial del of cost_function = (outputs - targets)^2 with respect to the output
 
     @staticmethod
-    def activationFunction(x):
-        return np.exp(x)/(np.exp(x)+1)  # this is basic sigmoid function
+    def activation_function(x):
+        return np.exp(x)/(np.exp(x)+1)  # sigmoid function
 
     @staticmethod
-    def s_prime(x):
-        return np.exp(x)/(np.exp(x)+1) - np.exp(2*x)/np.power((np.exp(x)+1), 2)
+    def activation_prime(x):
+        return np.exp(x)/(np.exp(x)+1) - np.exp(2*x)/np.power((np.exp(x)+1), 2)  # derivative of sigmoid
 
-    def feedForward(self, input):
+    def feedforward(self, input):
+        """empty the activation and zs values from previous feedforward"""
         self.activations = []
-        self.zs = []  # empty the node and bias values from previous feedforward
-        if len(input) == self.input_layer:
+        self.zs = []  # inputs to activation function
+        if len(input) == self.layer_sizes[0]:
             a = np.array(input)
             self.activations.append(a)
-            bias_index = 0
-            for w in self.weights:
-                w_a = np.matmul(w, a)  # W*A^(L-1)  ^:upper index, not power
-                z = np.subtract(w_a, self.biases[bias_index])  # W*A^(L-1)-B^L
+            for w, b in zip(self.weights, self.biases):
+                z = np.dot(w, a) + b
                 self.zs.append(z)
-                a = np.array(list(map(self.activationFunction, z)))  # sigmoid(Z)
+                a = self.activation_function(z)  # sigmoid(z)
                 self.activations.append(a)
-                bias_index += 1
             # return output
             return a
         else:
-            print('wrong input size')
-            return None
+            raise Exception('input size should be {}, input size was {}'.format(self.layer_sizes[0], len(input)))
 
-    def backpropagate(self, outputs, targets, eta):  # todo only calculate do not adjust, instead return deltas
-        errors = self.cost_prime(outputs, targets)  # Gc/Ga
-        for i in range(0, len(self.weights) - 1):
-            c_l = len(self.hidden_layers) - i  # index for current layer (indexing starts at zero)
-            # calculate gradients Ga/Gz*Gc/Ga
-            gradients = np.array(list(map(self.s_prime, self.zs[c_l])))
-            gradients = np.multiply(gradients, errors)
-            # calculate bias deltas and adjust biases
-            b_deltas = gradients
-            self.biases[c_l] = np.subtract(self.biases[c_l], eta*b_deltas)
-            # calculate weight deltas (i.e the amount we have to change the weights)
-            # this might look stupid, but its basically a dot product with activations transposed.
-            # numpy just made it a misery
-            w_deltas = np.dot(gradients.reshape(-1, 1), self.activations[c_l].reshape((1, -1)))
-            # adjust weights
-            self.weights[c_l] = np.subtract(self.weights[c_l], eta*w_deltas)
-            # and lets calculate the errors for the next layer
-            errors = np.matmul(np.transpose(self.weights[c_l]), gradients)
+    def backpropagate(self, outputs, targets):
+        error_gradients = self.cost_prime(outputs, targets)  # delC / delA
+        # for collection of the deltas
+        nabla_b = [np.zeros(l) for l in self.layer_sizes[1:self.num_layers]]
+        nabla_w = [np.zeros((l2, l1)) for l1, l2 in zip(self.layer_sizes[0:self.num_layers - 1],
+                                                        self.layer_sizes[1:self.num_layers])]
+        for i in range(1, self.num_layers):
+            gradients = error_gradients * self.activation_prime(self.zs[-i])  # delC / delA * delA / delZ
+            b_deltas = gradients  # since delZ / delB = 1
+            nabla_b[-i] = b_deltas
+            w_deltas = np.dot(gradients.reshape((-1, 1)), (self.activations[-i-1].reshape((-1, 1)).transpose()))
+            nabla_w[-i] = w_deltas
+            error_gradients = np.dot(np.transpose(self.weights[-i]), gradients)
+        return nabla_b, nabla_w
 
-    def train(self, inputs, targets, eta):
-        for i in range(1, 100):
-            outputs = self.feedForward(inputs)
-            self.backpropagate(outputs, targets, eta)
+    def train(self, inputs, targets):
+        outputs = self.feedforward(inputs)
+        return self.backpropagate(outputs, targets)
+
+    def train_databatch(self, inputs_matrix, targets_matrix, eta):
+        nabla_matrix = [self.train(inputs, targets) for inputs, targets in zip(inputs_matrix, targets_matrix)]
+        delta_b = np.average([x[0] for x in nabla_matrix], axis=0)
+        delta_w = np.average([x[1] for x in nabla_matrix], axis=0)
+        self.biases = [b - eta * d_b for b, d_b in zip(self.biases, delta_b)]
+        self.weights = [w - eta * d_w for w, d_w in zip(self.weights, delta_w)]
 
 
 if __name__ == '__main__':
-    nn = NeuralNetwork(4, [3, 3, 3], 2)
-    inputs = [1, 2, 3, 4]
-    untrained_output = nn.feedForward(inputs)
-    print(untrained_output)
-    for i in range(1, 100):
-        nn.train(inputs, [1, 0], 3)
-    print(nn.feedForward(inputs))
+    nn = NeuralNetwork([3, 3, 3, 2])
+    inputs_matrix = [[1, 0, 0], [0, 1, 0], [1, 1, 0], [0, 0, 0]]
+    targets_matrix = [[1, 1], [1, 1], [0, 1], [0, 0]]
+    for i in range(1, 10000):
+        nn.train_databatch(inputs_matrix, targets_matrix, 3)
+    print("{} compared to {}".format(nn.feedforward([1, 0, 0]), [1, 1]))
+    print("{} compared to {}".format(nn.feedforward([0, 1, 0]), [1, 1]))
+    print("{} compared to {}".format(nn.feedforward([1, 1, 0]), [0, 1]))
+    print("{} compared to {}".format(nn.feedforward([0, 0, 0]), [0, 0]))
+
+
